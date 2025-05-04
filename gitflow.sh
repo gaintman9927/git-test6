d7f907896a5c89442d5079d01087013c1da1b634
王鹏 <33058225+gaintman9927@users.noreply.github.com>
2025-05-04 20:13:11 +0800

    Update gitflow.sh

--- gitflow.sh
+++ gitflow.sh
@@ -3,6 +3,6 @@
  git branch  release2
  git branch  hotfix2
  git branch  hotfix21
- 
+ git branch  hotfix22
 #  git checkout -b  remotetomain origin/main                 
      # 从远程分支develop创建新本地分支devel并检出
