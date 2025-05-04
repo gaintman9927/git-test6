8a23de93a819773d60332afc9d358bee6321132f
paul <33058225+gaintman9927@users.noreply.github.com>
2025-05-04 19:57:50 +0800

    gitflow.sh

---
+++ gitflow.sh
@@ -0,0 +1,6 @@
+ git branch  develop2
+ git branch  feature2
+ git branch  release2
+ git branch  hotfix2
+#  git checkout -b  remotetomain origin/main                 
+     # 从远程分支develop创建新本地分支devel并检出
